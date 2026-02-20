3ef717f226c9a9217e082eca7ab9c65f0f7da856
Juan Jose <j.baezb@uniandes.edu.oc>
2026-02-19 23:26:44 -0500

    heuristica implementada sin prueba todavia

--- algorithms/heuristics.py
+++ algorithms/heuristics.py
@@ -43,4 +43,47 @@ def survivorHeuristic(state: Tuple[Tuple, Any], problem: MultiSurvivorProblem):
     - Balance heuristic strength vs. computation time (do experiments!)
     """
     # TODO: Add your code here
-    utils.raiseNotDefined()
+
+    position, survivors_grid = state
+    survivors = survivors_grid.asList()
+
+    # Si no quedan sobrevivientes
+    if len(survivors) == 0:
+        return 0
+
+    # re basica Distancia al sobreviviente más cercano 
+    manhattan_distances = []
+    for s in survivors:
+        d = abs(position[0] - s[0]) + abs(position[1] - s[1])
+        manhattan_distances.append(d)
+
+    nearest_distance = min(manhattan_distances)
+
+    # --- MST entre sobrevivientes restantes ---
+    # Usamos algoritmo tipo Prim sencillo
+
+    mst_cost = 0
+    visited = set()
+    remaining = set(survivors)
+
+    # Empezamos desde un sobreviviente cualquiera
+    current = survivors[0]
+    visited.add(current)
+    remaining.remove(current)
+
+    while remaining:
+        min_edge = float("inf")
+        next_node = None
+
+        for v in visited:
+            for r in remaining:
+                d = abs(v[0] - r[0]) + abs(v[1] - r[1])
+                if d < min_edge:
+                    min_edge = d
+                    next_node = r
+
+        mst_cost += min_edge
+        visited.add(next_node)
+        remaining.remove(next_node)
+
+    return nearest_distance + mst_cost
